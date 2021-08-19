28202cbc9acce4966e0bb20d96e7424bb600978a
vladyslav_krokha <vlad.strafe@gmail.com>
2021-08-19 14:31:29 +0300

    app

---
+++ src/app/core/pages/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  constructor() {
+    
+  }
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z0-9]{8,}')])
+  })
+}
